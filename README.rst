e0cfcfa37944fe1c7b83fa4b69d93689fdfc1394
Denis Podlesniy <haos616@gmail.com>
2015-05-06 19:18:31 +0300

    Add version 1.3.15 AngularJS

---
+++ README.rst
@@ -0,0 +1,60 @@
+Django AngularJS
+=============
+
+`AngularJS <https://angularjs.org/>`_ 1.3.15
+
+Requirements
+------------
+
+`Django <https://www.djangoproject.com/>`_ 1.4 or later
+
+Installation
+------------
+
+::
+
+    $ pip install django-static-angular==1.3.15
+
+Setup
+-----
+
+Just add ``'django.contrib.staticfiles'`` and ``'django_static_angular'`` to INSTALLED_APPS in
+your settings.py::
+
+    INSTALLED_APPS = (
+        # ...
+
+        'django.contrib.staticfiles',
+        'django_static_angular',
+
+        # ...
+    )
+
+Refer to Django `static files <https://docs.djangoproject.com/en/dev/howto/static-files/>`_
+documentation to configure and deploy static files.
+
+
+Usage
+-----
+
+You can refer to angular in your template with::
+
+    {% load staticfiles %}
+    {% static 'static_angular/js/angular.js' %}
+
+Admin template customization::
+
+    {% load staticfiles %}
+
+    {% extends "admin/base_site.html" %}
+
+    {% block extrahead %}
+        <script type="text/javascript" src="{% static 'static_angular/js/angular.js' %}" />
+    {% endblock %}
+
+Static files::
+
+    static_angular/js/angular.js - AngularJS - v1.3.15
+    static_angular/js/angular.min.js - AngularJS (min) - v1.3.15
+    static_angular/js/angular.min.map - AngularJS (map) - v1.3.15
+
---
+++ setup.py
@@ -0,0 +1,41 @@
+from os.path import join, dirname
+from distutils.core import setup
+
+try:
+    f = open(join(dirname(__file__), 'README.rst'))
+    long_description = f.read()
+    f.close()
+except IOError:
+    long_description = None
+
+setup(
+    name='django-static-angular',
+    version='1.3.15',
+    url="https://github.com/Haos616/django-static-angular",
+    description='AngularJS packaged in an handy django app to speed up new applications and deployment.',
+    long_description=long_description,
+    author='haos616',
+    author_email='haos616@gmail.com',
+    license='BSD',
+    keywords='django angular staticfiles'.split(),
+    platforms='any',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: BSD License',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Utilities',
+    ],
+    packages=['django_static_angular'],
+    package_data={
+        'django_static_angular': [
+            'static/static_angular/js/*.js',
+            'static/static_angular/js/*.map',
+        ],
+    },
+)
